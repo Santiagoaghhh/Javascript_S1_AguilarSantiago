2b7cd062dc2b2d6b028a6a9cc4a1d212786f8271
JuanPinilla16 <juanepinilla16@gmail.com>
2025-04-10 09:00:17 -0500

    feat: :fire: DIA1

---
+++ Dia1/camper.js
@@ -0,0 +1,47 @@
+var listaCampers = [
+    {
+        id: "1097099391",
+        nombres: "Santiago",
+        apellidos: "Aguilar",
+        direccion: "Calle 105 #15d-27",
+        acudiete: "Sandra Vesga",
+        telefono: "3502712359",
+        estado: "",
+        riesgo: "",
+    },
+    {
+        id: "",
+        nombres: "",
+        apellidos: "",
+        direccion: "",
+        acudiete: "",
+        telefono: "",
+        estado: "",
+        riesgo: "",
+    },
+    {
+        id: "",
+        nombres: "",
+        apellidos: "",
+        direccion: "",
+        acudiete: "",
+        telefono: "",
+        estado: "",
+        riesgo: "",
+    },
+    {
+        id: "",
+        nombres: "",
+        apellidos: "",
+        direccion: "",
+        acudiete: "",
+        telefono: "",
+        estado: "",
+        riesgo: "",
+    },
+
+]
+
+// MENÚ CAMPERS
+
+
---
+++ Dia1/mainMenu.js
@@ -0,0 +1,8 @@
+function ingresarUsuario(){
+    alert("Bienvenido a CampusLands")
+
+    alert("Ingesar como \n" +
+        "1. Camper \n" +
+        "2. Trainer \n" +
+        "3. Coordinador")
+}
